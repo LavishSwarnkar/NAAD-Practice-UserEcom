79cec75f8e0e97dea1251ed092ebe34b0f75b22a
Lavish Swarnkar <lavishswarnkar@gmail.com>
2020-11-28 20:05:09 +0530

    checkpoint 1

---
+++ app/src/main/java/com/lavish/android/userecom/services/MyFCMService.java
@@ -0,0 +1,9 @@
+package com.lavish.android.userecom.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFCMService extends FirebaseMessagingService {
+
+
+
+}
